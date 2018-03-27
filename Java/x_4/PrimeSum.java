d2de794dc4c155c00713b9eeebe2325764a87cb7
xianj <starnic@hotmail.com>
2018-03-27 16:52:34 +0800

    add

---
+++ Java/x_4/PrimeSum.java
@@ -0,0 +1,37 @@
+﻿import java.util.Scanner;
+
+/*
+ * 给定两个整数n和m，0<n<=m<=200
+ * 你的程序要计算第n个素数到第m个素数之间所有的素数的和，包括第n个素数和第m个素数。
+ */
+
+public class PrimeSum {
+
+	public static void main(String[] args) {
+		int cnt = 1;
+		int[] prime = new int[200];
+		prime[0] = 2;
+		//求出前200个素数并放入prime数组中
+		MAIN_LOOP: for (int x = 3; cnt < 200; x++) {
+			for (int j = 0; j < cnt; j++) {
+				if (x % prime[j] == 0) {
+					continue MAIN_LOOP;
+				}
+			}
+			prime[cnt++] = x;
+		}
+		
+		//读入两个数
+		Scanner in = new Scanner(System.in);	
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		int sum = 0;
+		//求出两个数之间的素数作为下标求和
+		for (int i = a-1; i <= b-1; i++) {
+			sum += prime[i];
+		}
+		System.out.println(sum);
+	}
+	
+}
---
+++ Java/x_4/SayDigits.java
@@ -0,0 +1,53 @@
+﻿import java.util.Scanner;
+
+/*
+ * 读入一个整数，范围是[-100000,100000]。然后，用汉语拼音将这个整数的每一位输出出来。
+ */
+public class SayDigits {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String num = in.nextLine();
+		StringBuffer nu = new StringBuffer();
+		//把-123456789的拼音放入到spellArr数组中
+		String[] spellArr = new String[] { "ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu", "fu" };
+		int len = num.length();
+		//创建一个新的数组arr
+		String[] arr = new String[len];
+		//判断输入的第一个字符是否是'-'
+		if (String.valueOf(num.charAt(0)).equals("-")) {
+			//如果是'-',就arr[0]置为10
+			arr[0] = "10";
+			//把输入的字符串存入arr数组中，下标从1开始了
+			for (int i = 1; i < len; i++) {
+				//把读入的数字字符串每一个字符取出来
+				arr[i] = String.valueOf(num.charAt(i));
+
+			}
+		} else {
+			//把输入的字符串存入arr数组中，下标从0开始
+			for (int i = 0; i < len; i++) {
+				arr[i] = String.valueOf(num.charAt(i));
+
+			}
+		}
+		
+		//利用arr数组中的数字把spellArr数组中的拼音写入Stringbuffer里
+		for (int i = 0; i < len; i++) {
+			//把arr数组的字符串元素转换为数字作为下标
+			int inte = Integer.parseInt(arr[i]);
+			if (i < len - 1) {
+				//最后一位之前的拼音都需要在后面加空格
+				nu.append(spellArr[inte] + " ");
+			} else {
+				//最后一位不需要
+				nu.append(spellArr[inte]);
+			}
+		}
+
+		System.out.println(nu);
+
+	}
+
+}
